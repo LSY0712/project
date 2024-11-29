f4bedf0cc4468ea9672da557a395b4721e802330
Catherine <catherine50733@gmail.com>
2024-11-29 09:52:34 +0800

    備份

--- back_login.php
+++ back_login.php
@@ -17,26 +17,26 @@
     <div class="container">
         <!-- Outer Row -->
         <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-4 col-md-9">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-4 d-none bg-login-image"></div>
+                            <div class="col">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="text-gray-900 mb-4">Dive In</h1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
                                             <input type="account" class="form-control form-control-user"
                                                 id="exampleInputAccount" aria-describedby="accountHelp"
-                                                placeholder="Account">
+                                                placeholder="帳號(Account)">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
-                                                id="exampleInputPassword" placeholder="Password">
+                                                id="exampleInputPassword" placeholder="密碼(Password)">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
@@ -60,9 +60,7 @@
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
 
     </div>
