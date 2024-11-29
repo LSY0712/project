16a9dcd6e26ade06ef84fab760ec5569ccb892c3
Catherine <catherine50733@gmail.com>
2024-11-29 11:58:51 +0800

    新增並修改內容

--- back_login.php
+++ back_login.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if(isset($_SESSION["user"])){
+    header("location: index.php");
+}
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -26,34 +34,41 @@
                             <div class="col">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="text-gray-900 mb-4">Dive In</h1>
+                                        <h1 class="text-gray-900 mb-4">Dive In 後台</h1>
                                     </div>
-                                    <form class="user">
+                                    <?php if(isset($_SESSION["error"]["times"]) && $_SESSION["error"]["times"]>10): ?>
+                                    <h3 class="text-danger">輸入錯誤次數太多, 請稍後再來</h3>
+                                    <?php else: ?>
+                                    <form action="doBack_login.php" method="POST" class="user">
                                         <div class="form-group">
-                                            <input type="account" class="form-control form-control-user"
+                                            <input type="text" class="form-control form-control-user" name="account"
                                                 id="exampleInputAccount" aria-describedby="accountHelp"
                                                 placeholder="帳號(Account)">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
+                                            <input type="password" class="form-control form-control-user" name="password"
                                                 id="exampleInputPassword" placeholder="密碼(Password)">
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">記住我</label>
+                                                <input type="checkbox" class="form-check-input" id="customCheck">
+                                                <label class="form-check-label" for="remember">記住我</label>
                                             </div>
                                         </div>
-                                        <a href="index.php" class="btn btn-primary btn-user btn-block">
-                                            登入
-                                        </a>
+                                        <?php if (isset($_SESSION["error"]["message"])): ?>
+                                            <div class="p-1 mb-2 text-danger bg-info bg-gradient"><?= htmlspecialchars($_SESSION["error"]["message"]) ?></div>
+                                        <?php unset($_SESSION["error"]["message"]); endif; ?>
+                                        <div class="d-grid">
+                                        <button class="btn btn-info btn-user btn-block" type="submit">登入</button>
+                                        </div>
                                     </form>
+                                    <?php endif; ?>
                                     <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">忘記密碼?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">註冊帳號</a>
+                                        <a class="small" href="register.php">註冊帳號！</a>
                                     </div>
                                 </div>
                             </div>
